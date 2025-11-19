9c07d6a92e29e9791e10e0549b1644ee4cfc6af4
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-19 10:36:10 +0530

    Two pointer apporach to solve product of array except self problem

---
+++ Arrays/Medium/product_of_array_except_self.java
@@ -0,0 +1,19 @@
+//problem link: https://leetcode.com/problems/product-of-array-except-self/description/
+//time complexity: O(n)
+//space complexity: O(n)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int[] result = new int[nums.length];
+        Arrays.fill(result, 1);
+        int pre = 1, post = 1;
+        for(int i = 0; i < nums.length; i++){
+            result[i] = pre;
+            pre = nums[i] * pre;
+        }
+        for(int i = nums.length - 1; i >= 0; i--){
+            result[i] *= post;
+            post = nums[i] * post;
+        }
+        return result;
+    }
+}
