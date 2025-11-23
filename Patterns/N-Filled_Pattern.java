f09b530e355aa1873fa5443dda83615367bf81f7
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-23 13:22:39 +0530

    Filled rectangle Pattern

---
+++ Patterns/N-Filled_Pattern.java
@@ -0,0 +1,17 @@
+/*
+n = 4
+* * * *
+* * * *
+* * * *
+* * * *
+ */
+class Solution{
+    public static void printPattern(int n){
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("*" + " ");
+            }
+            System.out.println();
+        }
+    }
+}
