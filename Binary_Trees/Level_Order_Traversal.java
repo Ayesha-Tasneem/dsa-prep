b213a10d474918fac9aa5241466df6a5fc8a5744
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-24 09:23:20 +0530

    BFS traversal for binary trees

---
+++ Binary_Trees/Level_Order_Traversal.java
@@ -0,0 +1,27 @@
+//problem link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+//time complexity: O(n)
+//space complexity: O(n)
+//level order - bfs
+class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (root == null)
+            return result;
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        while (!q.isEmpty()) {
+            int size = q.size();
+            List<Integer> level = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                TreeNode curr = q.poll();
+                level.add(curr.val);
+                if (curr.left != null)
+                    q.offer(curr.left);
+                if (curr.right != null)
+                    q.offer(curr.right);
+            }
+            result.add(level);
+        }
+        return result;
+    }
+}
