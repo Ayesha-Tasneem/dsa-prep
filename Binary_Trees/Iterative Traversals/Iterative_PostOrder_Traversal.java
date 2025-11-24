e34798ae12c6e1bee1c0d0596bfd851262de1e22
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-24 13:32:32 +0530

    1 stack and 2 stack approach for post order traversal

--- Binary_Trees/Iterative Traversals/Iterative_PostOrder_Traversal.java
+++ Binary_Trees/Iterative Traversals/Iterative_PostOrder_Traversal.java
@@ -2,6 +2,8 @@
 //time complexity: O(n)
 //space complexity: O(n)
 //post order traversal: left -> right -> root
+
+//two stack
 class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
@@ -18,6 +20,33 @@ class Solution {
         while(!st2.isEmpty()){
             result.add(st2.pop().val);
         }
+        return result;
+    }
+}
+
+//single stack
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        TreeNode lastVisited = null;
+
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            TreeNode peekNode = stack.peek();
+            if (peekNode.right == null || peekNode.right == lastVisited) {
+                result.add(peekNode.val);
+                lastVisited = peekNode;
+                stack.pop();
+            } else {
+                curr = peekNode.right;
+            }
+        }
+
         return result;
     }
 }
