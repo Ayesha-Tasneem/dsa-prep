e20c83f9d90192e60ac52ad69ea9c560e0575131
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-25 09:34:41 +0530

    middle of the linked list - Leetcode 876

---
+++ Linked List/middle_of_list.java
@@ -0,0 +1,15 @@
+//problem link: https://leetcode.com/problems/middle-of-the-linked-list/description/
+//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        if(head.next == null) return head;
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
