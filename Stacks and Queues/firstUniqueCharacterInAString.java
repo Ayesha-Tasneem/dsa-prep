51527599fe2258819c16fceaf1a3bd7431cc2575
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-27 10:58:24 +0530

    First Unique Character in a String using Queue

---
+++ Stacks and Queues/firstUniqueCharacterInAString.java
@@ -0,0 +1,18 @@
+//problem link: https://leetcode.com/problems/first-unique-character-in-a-string/description/
+//time complexity: O(n)
+//space complexity: O(n)
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+        Queue<Integer> q = new LinkedList<>();
+        for(int i = 0; i < s.length(); i++){
+            char c = s.charAt(i);
+            freq[c - 'a']++;
+            q.offer(i);
+            while(!q.isEmpty() && freq[s.charAt(q.peek()) - 'a'] > 1){
+                q.poll();
+            }
+        }
+        return q.isEmpty() ? -1 : q.peek();
+    }
+}
