7650ca4a9d013a6ec7e08a5a98c971568ef5d4d0
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-11-27 14:44:41 +0530

    Reverse a linked list only between given values left,right

---
+++ Linked List/reverseLinkedListII.java
@@ -0,0 +1,23 @@
+//problem link: https://leetcode.com/problems/reverse-linked-list-ii/description/
+//time complexity: O(n)
+//space complexity: O(1)
+class Solution {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left == right) return head;
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for(int i = 1; i < left; i++){
+            prev = prev.next;
+        }
+        ListNode curr = prev.next;
+        ListNode next = null;
+        for(int i = 0; i < right - left; i++){
+            next = curr.next;
+            curr.next = next.next;
+            next.next = prev.next;
+            prev.next = next;
+        }
+        return dummy.next;
+    }
+}
