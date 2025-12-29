4a175810e7de33016e97a1c686c36887f981fa6e
Ayesha-Tasneem <ayeshatasneems2005@gmail.com>
2025-12-29 17:51:34 +0530

    Array Leetcode Questions

---
+++ Arrays/Easy/twoSum.java
@@ -0,0 +1,18 @@
+//problem link: https://leetcode.com/problems/two-sum/description
+//time complexity: O(n)
+//space complexity: O(n)
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++){
+            int diff = target - nums[i];
+            if(map.containsKey(diff)){
+                return new int[]{map.get(diff),i};
+            }
+            else{
+                map.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+}
